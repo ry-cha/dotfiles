b10356e5465a553fe1b3219de74e587bea9196fd
chanr335 <76451685+chanr335@users.noreply.github.com>
2026-01-09 15:10:37 -0500

    lsp update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,3 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
-
-local notify = vim.notify
-vim.notify = function(msg, ...)
-    if msg:match("warning: multiple different client offset_encodings")
-        or msg:match("position_encoding param is required in vim.lsp.util.make_position_params") then
-        return
-    end
-
-    notify(msg, ...)
-end
--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,85 +1,41 @@
-return {
-    "neovim/nvim-lspconfig",
-    event = {"BufReadPre", "BufNewFile"},
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        {"antosha417/nvim-lsp-file-operations", config = true},
-        {"folke/lazydev.nvim", config = true},
-    },
-    config = function()
-        local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        lspconfig = require('lspconfig')
-            lspconfig.sourcekit.setup({
-                capabilities = {
-                    workspace = {
-                        didChangeWatchedFiles = {
-                        dynamicRegistration = true,
-                    },
+return {{
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    opts = {
+        signature = { enabled = true },
+        completion = {
+            menu = {
+                draw = {
+                    columns = { { "label", "label_description", gap = 1 }, { "kind" } },
                 },
             },
-        })
-
-        local servers = {
-            "ts_ls",
-            "html",
-            "cssls",
-            "tailwindcss",
-            "lua_ls",
-            "clangd",
-            "rust_analyzer",
-        }
-
-        for _, server in ipairs(servers) do
-                lspconfig[server].setup({
-                    capabilities = capabilities,
-                })
-            end
-
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Show LSP references"
-                })
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {
-                    buffer = ev.buf, silent = true, desc = "Go to declaration"
-                })
-                vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Show LSP definitions"
-                })
-                vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Show LSP implementations"
-                })
-                vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Show LSP type definitions"
-                })
-                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {
-                    buffer = ev.buf, silent = true, desc = "See available code actions"
-                })
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {
-                    buffer = ev.buf, silent = true, desc = "Smart rename"
-                })
-                vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Show buffer diagnostics"
-                })
-                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
-                    buffer = ev.buf, silent = true, desc = "Show line diagnostics"
-                })
-                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {
-                    buffer = ev.buf, silent = true, desc = "Go to previous diagnostic"
-                })
-                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {
-                    buffer = ev.buf, silent = true, desc = "Go to next diagnostic"
-                })
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, {
-                    buffer = ev.buf, silent = true, desc = "Show documentation for what is under cursor"
-                })
-                vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", {
-                    buffer = ev.buf, silent = true, desc = "Restart LSP"
-                })
-            end,
-        })
-    end,
+        },
+    },
+},
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "saghen/blink.cmp",
+            { "folke/lazydev.nvim", config = true },
+        },
+        config = function()
+            require("mason").setup()
+            local lspconfig = require("lspconfig")
+            local blink = require("blink.cmp")
+            local capabilities = blink.get_lsp_capabilities()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                },
+            })
+        end,
+    },
 }
